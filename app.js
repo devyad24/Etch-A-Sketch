7603cc8e0be3aeec32bafa8ee693ba200b238da2
devyad24 <devyadav4211@gmail.com>
2022-03-08 12:04:40 +0530

    Add EventListeners on grid

--- app.js
+++ app.js
@@ -5,15 +5,27 @@ container.style.display = 'grid';
 container.style.gridTemplateColumns = 'repeat(16,1fr)';
 container.style.marginTop = '50px';
 for(let i = 0;i<256;i++){
+   makeGrid(i);
+   
+}
+function makeGrid(a){
     const div = document.createElement('div');
     div.style.width = '50px';
     div.style.height = '50px';
     div.style.border = '1px solid blue';
-    div.textContent = `${i+1}`;
+    div.textContent = `${a+1}`;
     div.classList.add('block');
     container.appendChild(div);
 }
-const allDivs = document.querySelectorAll('.block');
-allDivs.forEach((e)=>{
-    e.addEventListener('mouseover',a=>a.target.style.backgroundColor = 'aqua');
+const allBlocks = document.querySelectorAll('.block');
+allBlocks.forEach((block)=>{
+    block.addEventListener('mouseenter',handleEvent);
+    block.addEventListener('mouseleave',(e)=>{
+       setTimeout(()=>{
+        e.target.style.backgroundColor = '#fff';
+       },400);
+    })
 })
+function handleEvent(e){
+    e.target.style.backgroundColor = '#4cc9f0';
+}
